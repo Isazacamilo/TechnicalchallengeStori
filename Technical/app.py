a9d22dd92a070b97ab455d9c284e5cc44fa2a239
unknown <camilo>
2023-09-09 17:53:40 -0500

    Dockerfile

--- Technical/app.py
+++ Technical/app.py
@@ -2,7 +2,7 @@ from process_csv.process_csv import process_csv, process_csv_to_database
 from mail.send_notification import send_notification
 
 def main():
-    transaction_account = 'Transactions/account.csv'
+    transaction_account = 'transactions/account.csv'
     transactions_details = process_csv(transaction_account)
     process_csv_to_database(transaction_account)
     send_notification(*transactions_details)
